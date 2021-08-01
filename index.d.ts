7780d31f581d687a28ac62b4bd75c28a526d8b0c
quapo <max@nagele.dev>
2021-08-01 20:01:42 +0200

    Updated ts

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,18 @@
+interface loadResult {
+    [keys: string] : string | Error;
+    error ?: Error;
+}
+
 /**
  * Loads the config file from the working directory.
  * @param configFileName - optional name of the config file defaults to jenvcfg.json
- * @returns boolean - true if the config file was loaded successfully
+ * @returns loadResult - true if the config file was loaded successfully
  */
-export function load(configFileName?: String): Boolean;
+export function load(configFileName?: String): loadResult;
 
 /**
  * Loads the config file depending on the current NODE_ENV from the working directory.
  * Path to the file should be [NODE_ENV].jenvcfg.json.json
- * @returns boolean - true if the config file was loaded successfully
+ * @returns loadResult - true if the config file was loaded successfully
  */
-export function loadByEnv(): Boolean;
+export function loadByEnv(): loadResult;
